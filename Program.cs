c23ea40fc62fab40a63032f5b276d27e6e873a3b
AnonymousDonut <172885213+AnonymousDonut@users.noreply.github.com>
2025-08-26 22:46:59 +0300

    wawa
    add source code

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+const int SPI_SETDESKWALLPAPER = 0x0014;
+const int SPIF_UPDATEINIFILE   = 0x01;
+const int SPIF_SENDCHANGE      = 0x02;
+
+var Dir = AppDomain.CurrentDomain.BaseDirectory;
+var DaysDir = Path.Combine(Dir, "Days");
+var theday =  DateTime.Today.DayOfWeek;
+
+if (!Directory.Exists(DaysDir))
+{
+    Console.WriteLine("Directory doesn't exist");
+}
+
+Console.WriteLine(WallpaperForDay());
+
+string WallpaperForDay()
+{
+    if (theday == DayOfWeek.Sunday)
+    {
+        return "Sunday";
+    }
+    if (theday == DayOfWeek.Monday)
+    {
+        return "Monday";
+    }
+    if (theday == DayOfWeek.Tuesday)
+    {
+        return "Tuesday";
+    }
+    if (theday == DayOfWeek.Wednesday)
+    {
+        return "Wednesday";
+    }
+    if (theday == DayOfWeek.Thursday)
+    {
+        return "Thursday";
+    }
+    if (theday == DayOfWeek.Friday)
+    {
+        return "Friday";
+    }
+    if (theday == DayOfWeek.Saturday)
+    {
+        return "Saturday";
+    }
+
+    return "Error";
+}
+
+void ChangeWallpaper()
+{
+    using (var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", writable: true)!)
+    {
+        key.SetValue("WallpaperStyle", "10");
+        key.SetValue("TileWallpaper", "0");
+        
+        // This is all just ai garbage btw
+    }
+    if (!SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, $@"{DaysDir}\{WallpaperForDay()}.png", SPIF_UPDATEINIFILE | SPIF_SENDCHANGE))
+    {
+        throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error(), "Failed to change wallpaper");
+    }
+}
+
+ChangeWallpaper();
